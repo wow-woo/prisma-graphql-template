32706d9b7847faa14c6adb1c3a7e9bf768d301aa
wow-woo <fwpass@outlook.com>
2021-05-30 09:44:44 +0900

    graphql trainning and introduction

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
-const { gql, ApolloServer } = require("apollo-server");
+import { gql, ApolloServer } from "apollo-server";
 
 const typeDefs = gql`
+  type Buffet {
+    appetite: String
+    main: String
+  }
   type Query {
-    hello: String
+    morning: String
+    launch: String
+    dinner: Buffet
+    receipt: [Buffet]
+  }
+
+  input Din {
+    name: String
+    price: Int
+  }
+
+  type Mutation {
+    changeDinner(menu: Din): Buffet
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => {
-      return "world";
+    dinner: () => ({
+      appetite: "soup",
+      main: "chicken",
+    }),
+
+    receipt: () => [
+      { appetite: "01", main: "01main" },
+      { appetite: "02", main: "02main" },
+      { appetite: "03", main: "03main" },
+    ],
+  },
+
+  Mutation: {
+    changeDinner: (_, { menu }) => {
+      return { appetite: null, main: menu.name };
     },
   },
 };
